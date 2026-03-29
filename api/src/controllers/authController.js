1755a4b35f6d085953293af3129da1049bf0ccc1
pradashades333 <alexdreve03@gmail.com>
2026-03-29 16:04:28 +0200

    fixed register route

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -1,15 +1,20 @@
 const prisma = require("../lib/prisma");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
     register: async (req, res, next) => {
         try {
-            const {email, password,username } = req.body;
-            const hashedPass = await bcrypt.hash(password,10)
-            await prisma.user.create({data :{email,username, password:hashedPass}})
-            res.json({ message: "User created" })
+        const { email, password, username } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await prisma.user.create({
+            data: { email, username, password: hashedPassword },
+        });
+        res.json({ message: "User created" });
         } catch (err) {
-            next(err)
+        next(err);
         }
-    }
+    },
+
+    
 }
--- api/src/index.js
+++ api/src/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-const authRouter = require('./routes/auth')
-const commentsRouter = require('./routes/posts')
-const postsRouter = require('./routes/posts')
-app.use(authRouter)
-app.use(commentsRouter)
-app.use(postsRouter)
+
+const authRouter = require('./routes/auth');
+const postsRouter = require('./routes/posts');
+const commentsRouter = require('./routes/comments');
+
+app.use("/auth", authRouter);
+app.use("/posts", postsRouter);
+app.use("/posts", commentsRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "Blog API is running" });
