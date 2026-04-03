88a4771a93de5b9486a54b39d56d25bf483df0eb
pradashades333 <alexdreve03@gmail.com>
2026-04-03 21:38:03 +0200

    started post

---
+++ frontend/src/pages/Post.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+function Register() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+    e.preventDefault();
+        fetch('http://localhost:3000/auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password , username})
+        })
+        .then(() => navigate('/login'))
+    }
+
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Email:
+                <input type = "email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            </label>
+            <label>Username:
+                <input type = "text" value = {username} onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <label>Password:
+                <input type = "password" value = {password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <button type = "submit" > Log-in</button>
+
+        </form>
+    )
+
+}
+
+export default Register;
