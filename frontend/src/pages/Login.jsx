7e510e5087ce5621f2e5f99f247da7a239f31b1e
pradashades333 <alexdreve03@gmail.com>
2026-04-05 23:46:34 +0200

    added basic css to login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,16 +17,20 @@ function Login() {
 
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Email:
-                <input type = "email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-            </label>
-            <label>Password:
-                <input type = "password" value = {password} onChange={(e) => setPassword(e.target.value)} />
-            </label>
-            <button type = "submit" > Log-in</button>
-
-        </form>
+        <div style={{ backgroundColor: 'lightblue', padding: '20px' }}>
+            <h1 style={{ color: 'navy', textAlign: 'center' }}>Login</h1>
+            <form onSubmit={handleSubmit} style={{ backgroundColor: 'white', border: '2px solid navy', padding: '10px' }}>
+                <label>Email:
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} style={{ marginLeft: '5px', marginBottom: '10px' }}/>
+                </label>
+                <br />
+                <label>Password:
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ marginLeft: '5px', marginBottom: '10px' }} />
+                </label>
+                <br />
+                <button type="submit" style={{ backgroundColor: 'navy', color: 'white', padding: '5px 10px' }}>Log-in</button>
+            </form>
+        </div>
     )
 
 }
