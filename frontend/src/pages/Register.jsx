10f632e27242912095cc38449d4f079c44c773bd
pradashades333 <alexdreve03@gmail.com>
2026-04-05 23:56:01 +0200

    added basic css to register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -21,19 +21,24 @@ function Register() {
 
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Email:
-                <input type = "email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-            </label>
-            <label>Username:
-                <input type = "text" value = {username} onChange={(e) => setUsername(e.target.value)} />
-            </label>
-            <label>Password:
-                <input type = "password" value = {password} onChange={(e) => setPassword(e.target.value)} />
-            </label>
-            <button type = "submit" > Log-in</button>
-
-        </form>
+        <div style={{ backgroundColor: 'lightblue', padding: '20px' }}>
+            <h1 style={{ color: 'navy', textAlign: 'center' }}>Register</h1>
+            <form onSubmit={handleSubmit} style={{ backgroundColor: 'white', border: '2px solid navy', padding: '10px' }}>
+                <label>Email:
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} style={{ marginLeft: '5px', marginBottom: '10px' }} />
+                </label>
+                <br />
+                <label>Username:
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} style={{ marginLeft: '5px', marginBottom: '10px' }} />
+                </label>
+                <br />
+                <label>Password:
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ marginLeft: '5px', marginBottom: '10px' }} />
+                </label>
+                <br />
+                <button type="submit" style={{ backgroundColor: 'navy', color: 'white', padding: '5px 10px' }}>Register</button>
+            </form>
+        </div>
     )
 
 }
