4c15d1ec9093f4e7a4bc59d7d97553a57b2e223e
pradashades333 <alexdreve03@gmail.com>
2026-04-09 17:08:42 +0200

    improved css for new post

--- author/src/pages/NewPost.jsx
+++ author/src/pages/NewPost.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import './NewPost.css'
 
 function NewPost() {
   const [title, setTitle] = useState('')
@@ -26,23 +27,27 @@ function NewPost() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Title:
-        <input type="text" value={title} onChange={e => setTitle(e.target.value)}
-        />
-      </label>
-
-      <label>
-        Content:
-        <textarea
-          value={content}
-          onChange={e => setContent(e.target.value)}
-        />
-      </label>
-
-      <button type="submit">Create Post</button>
-    </form>
+    <div className="new-post">
+      <h1>New Post</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title
+          <input
+            type="text"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        </label>
+        <label>
+          Content
+          <textarea
+            value={content}
+            onChange={e => setContent(e.target.value)}
+          />
+        </label>
+        <button type="submit">Create Post</button>
+      </form>
+    </div>
   )
 }
 
