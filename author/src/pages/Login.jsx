17c5b0a3e3accd54bce37e27546181310f547872
pradashades333 <alexdreve03@gmail.com>
2026-04-09 16:35:06 +0200

    improved login css

--- author/src/pages/Login.jsx
+++ author/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import './Login.css'
 
 function Login() {
   const [email, setEmail] = useState('')
@@ -26,27 +27,30 @@ function Login() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input
-          type="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-      </label>
-
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </label>
-
-      <button type="submit">Log in</button>
-    </form>
+    <div className="login-page">
+      <div className="login-card">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Email
+            <input
+              type="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </label>
+          <button type="submit">Log in</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
